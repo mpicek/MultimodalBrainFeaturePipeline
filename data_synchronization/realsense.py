8defcce1239c96e00e7bdb71768272ecfed264d2
mpicek <martinpicek4@gmail.com>
2024-03-08 17:04:58 +0100

    Transform cam. space to face space

--- data_synchronization/realsense.py
+++ data_synchronization/realsense.py
@@ -23,8 +23,20 @@ def extract_face_vector(path_bag, mediapipe_face_model_file):
     - mediapipe_face_model_file (str): Path to the Mediapipe face model file for face detection.
 
     Returns:
-    - numpy.ndarray: An array of shape (frames, 3)
-      Each row contains the x, y, z vector direction relative to another facial point.
+    tuple: forehead_points, quality, face2cam, cam2face, face_coordinates_origin, duration
+        - forehead_points (numpy.ndarray): An array of shape (frames, 3)
+              Each row contains the x, y, z coordinates (in camera space) of a point on the forehead.
+              The magnitude should be normalized so that all three coordinates have the same scale (like in 3d).
+              (because Mediapipe returns the coordinates in the range [0, 1] relative to the
+               height and width of the image, so we multiplied it by the width and height of the image)
+        - quality (numpy.ndarray): An array of shape (frames,) that is 1 if mediapipe detected
+              the face and 0 otherwise. This is used to indicate the quality of the forehead_points.
+        - face2cam (numpy.ndarray): A 3x3 matrix representing the transformation from face to camera coordinates.
+        - cam2face (numpy.ndarray): A 3x3 matrix representing the transformation from camera to face coordinates.
+        - face_coordinates_origin (numpy.ndarray): A 3x1 vector representing the origin of the face coordinate system.
+              Expressed in the camera coordinate system.
+        - duration (float): The duration of the video in seconds.
+
 
     Notes:
     - The function assumes a video resolution of 640x480.
@@ -72,10 +84,14 @@ def extract_face_vector(path_bag, mediapipe_face_model_file):
     ts = 0
 
     time_series = []
-    nose_stacked = []
+    forehead_points, quality = [], []
     duration = playback.get_duration().total_seconds() * 1000
     print(f"Overall video duration: {playback.get_duration()}")
+
     # playback.seek(datetime.timedelta(seconds=73*4))
+
+    face_coordinate_system = None
+
     try:
         while True:
             try:
@@ -145,15 +161,77 @@ def extract_face_vector(path_bag, mediapipe_face_model_file):
             image = mp.Image(image_format=mp.ImageFormat.SRGB, data=cv2.cvtColor(face_image, cv2.COLOR_BGR2RGB))
             detection_result = detector.detect(image)
             try:
-                nose_stacked.append(np.array([
-                    detection_result.face_landmarks[0][8].x - detection_result.face_landmarks[0][2].x,
-                    detection_result.face_landmarks[0][8].y - detection_result.face_landmarks[0][2].y,
-                    detection_result.face_landmarks[0][8].z - detection_result.face_landmarks[0][2].z,
+                # Detect the point on forehead (and z is relative, but we don't care about it because a person
+                # moves the head in an "angular" way and not back and forth. And the point is on the forehead,
+                # so the angle of the head is going to be very similar to the z coordinate. It's an approximation
+                # that should be good enough for our purposes)
+                # We need to normalize the coordinates because mediapipe returns just the relative coordinates to the
+                # size of the image. So we multiply it by the width and height of the image (cropped area only in our case
+                # as mediapipe operates only on the cropped image!!). And, according to mediapipe docs, the z coordinate
+                # is approximately of the same magnitude as x
+                forehead_points.append(np.array([
+                    detection_result.face_landmarks[0][8].x * color_image_rs.shape[1],
+                    detection_result.face_landmarks[0][8].y * color_image_rs.shape[0],
+                    detection_result.face_landmarks[0][8].z * color_image_rs.shape[1],
                 ]))
+
+                quality.append(1)
+
+                if face_coordinate_system is None:
+                    # We find the face coordinate system in the first detected frame (this will be the reference)
+                    # It's gonna be in matrix [x, y, z] where x, y, z are the bases of the coordinate system
+                    # they are normalized. X and Y don't have to be necessarily orthogonal, but we assume they are
+                    # z is orthogonal to x and y
+
+                    # it's a nose point :)
+                    face_coordinate_system = True
+
+                    face_coordinates_origin = np.array([
+                        detection_result.face_landmarks[0][1].x * color_image_rs.shape[1],
+                        detection_result.face_landmarks[0][1].y * color_image_rs.shape[0],
+                        detection_result.face_landmarks[0][1].z * color_image_rs.shape[1],
+                    ])
+
+                    nose_vector = np.zeros((3,))
+
+                    nose_vector[0] = (detection_result.face_landmarks[0][8].x - detection_result.face_landmarks[0][2].x)
+                    nose_vector[1] = (detection_result.face_landmarks[0][8].y - detection_result.face_landmarks[0][2].y)
+                    nose_vector[2] = (detection_result.face_landmarks[0][8].z - detection_result.face_landmarks[0][2].z)
+                    nose_vector = nose_vector / np.linalg.norm(nose_vector)
+                    
+                    eye_vector = np.zeros((3,))
+                    eye_vector[0] = (detection_result.face_landmarks[0][33].x - detection_result.face_landmarks[0][263].x)
+                    eye_vector[1] = (detection_result.face_landmarks[0][33].y - detection_result.face_landmarks[0][263].y)
+                    eye_vector[2] = (detection_result.face_landmarks[0][33].z - detection_result.face_landmarks[0][263].z)
+                    eye_vector = eye_vector / np.linalg.norm(eye_vector)
+
+                    # we want to find the orthogonalized eye vector - Let's use Gram Schmidt process
+                    # so we subtract the projection of the eye vector to the nose vector from the eye vector
+                    # projection is <v1 . v2> * v1 (it's how much it's projeceted times v1 which is the direction
+                    # of the projection). This will be subtracted from the original vector to get the orthogonalized:
+                    # v2_orth = v2 - <v1 . v2> * v1
+                    # (and we suppose that both vectors are normalized, so we don't have to divide by the norms)
+                    eye_vector_orthogonalized = eye_vector - np.dot(eye_vector, nose_vector) * nose_vector
+                    # and normalize it to be sure
+                    eye_vector_orthogonalized = eye_vector_orthogonalized / np.linalg.norm(eye_vector_orthogonalized)
+
+                    third_orthogonal_vector = np.cross(nose_vector, eye_vector_orthogonalized)
+                    third_orthogonal_vector = third_orthogonal_vector / np.linalg.norm(third_orthogonal_vector)
+
+                    # this would transform points in face coordinates to the camera coordinates
+                    face2cam = np.column_stack((nose_vector, eye_vector_orthogonalized, third_orthogonal_vector))
+
+                    # this would transform points in camera coordinates to the face coordinates
+                    # it's gonna have an inverse as it was a orthonormal matrix (it's actually its transpose..)
+                    cam2face = np.linalg.inv(face2cam)
+
             except:
                 print("Mediapipe didn't detect a thing! Using the last valid values.")
-                if len(nose_stacked) > 0:
-                    nose_stacked.append(nose_stacked[-1])
+                if len(forehead_points) > 0:
+                    forehead_points.append(forehead_points[-1])
+                    quality.append(0)
+                else:
+                    duration -= 1000/30 # to account for the lost frames at the beginning
 
             # STEP 5: Process the detection result. In this case, visualize it.
             annotated_image = draw_face_landmarks_on_image(image.numpy_view(), detection_result)
@@ -164,6 +242,9 @@ def extract_face_vector(path_bag, mediapipe_face_model_file):
 
             if prev_ts - prev_prev_ts > 2*int(1000/30) - 10:
                 print(f"Skipped frame. Previous ts: {prev_ts}, difference {prev_ts - prev_prev_ts}")
+                if len(forehead_points) > 0:
+                    forehead_points.append(forehead_points[-1])
+                    quality.append(0)
 
             time_series.append([prev_ts])
 
@@ -178,10 +259,10 @@ def extract_face_vector(path_bag, mediapipe_face_model_file):
 
             if frame_nb > 100:
                 ax.cla()
-                indices = np.arange(len(nose_stacked))
-                x = smooth(np.gradient(np.array(nose_stacked)[:, 0]), 20)
-                y = smooth(np.gradient(np.array(nose_stacked)[:, 1]), 20)
-                z = smooth(np.gradient(np.array(nose_stacked)[:, 2]), 20)
+                indices = np.arange(len(forehead_points))
+                x = smooth(np.gradient(np.array(forehead_points)[:, 0]), 20)
+                y = smooth(np.gradient(np.array(forehead_points)[:, 1]), 20)
+                z = smooth(np.gradient(np.array(forehead_points)[:, 2]), 20)
                 ax.plot(indices, x, label='x')
                 ax.plot(indices, y, label='y')
                 ax.plot(indices, z, label='z')
@@ -191,14 +272,31 @@ def extract_face_vector(path_bag, mediapipe_face_model_file):
     finally:
         pipeline.stop()
         cv2.destroyAllWindows()
-        nose_stacked = np.stack(nose_stacked)
+        forehead_points = np.stack(forehead_points)
+        quality = np.stack(quality)
     
-    return nose_stacked
+    return forehead_points, quality, face2cam, cam2face, face_coordinates_origin, duration/1000
 
 if __name__ == '__main__':
-    path_bag = "/home/mpicek/repos/master_project/test_data/corresponding/cam0_911222060374_record_13_11_2023_1330_19.bag"
-    # path_bag = "/home/mpicek/repos/master_project/test_data/corresponding/cam0_911222060374_record_13_11_2023_1337_20.bag"
-    nose_vector_data = extract_face_vector(path_bag, mediapipe_face_model_file = '/home/mpicek/Downloads/face_landmarker.task')
-    output_filename = 'nose_vector.npy'
-    # print(f"Saving data of shape {nose_vector_data.shape} to {output_filename}")
-    # np.save(output_filename, nose_vector_data)
+
+    # path_bag = "/home/mpicek/repos/master_project/test_data/corresponding/cam0_911222060374_record_13_11_2023_1330_19.bag"
+    path_bag = "/home/mpicek/repos/master_project/test_data/corresponding/cam0_911222060374_record_13_11_2023_1337_20.bag"
+    forehead_points, quality_data, face2cam, cam2face, face_coordinates_origin, duration = extract_face_vector(path_bag, mediapipe_face_model_file = '/home/mpicek/Downloads/face_landmarker.task')
+    output_filename = 'forehead_points_20.npy'
+    quality_output_filename = 'quality_20.npy'
+    face2cam_output_filename = 'face2cam_20.npy'
+    cam2face_output_filename = 'cam2face_20.npy'
+    face_coordinates_origin_output_filename = 'face_coordinates_origin_20.npy'
+    duration_output_filename = 'duration_20.npy'
+    print(f"Saving data of shape {forehead_points.shape} to {output_filename}")
+    print(f"Saving quality data of shape {quality_data.shape} to {quality_output_filename}")
+    print(f"Saving face2cam data of shape {face2cam.shape} to {face2cam_output_filename}")
+    print(f"Saving cam2face data of shape {cam2face.shape} to {cam2face_output_filename}")
+    print(f"Saving face_coordinates_origin data of shape {face_coordinates_origin.shape} to {face_coordinates_origin_output_filename}")
+    print(f"Saving duration data to {duration_output_filename}")
+    np.save(output_filename, forehead_points)
+    np.save(quality_output_filename, quality_data)
+    np.save(face2cam_output_filename, face2cam)
+    np.save(cam2face_output_filename, cam2face)
+    np.save(face_coordinates_origin_output_filename, face_coordinates_origin)
+    np.save(duration_output_filename, duration)
