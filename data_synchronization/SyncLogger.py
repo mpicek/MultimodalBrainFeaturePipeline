bb82bebb81d437966487c63ed20128e25c7492be
Martin Picek <martinpicek4@gmail.com>
2024-04-05 17:48:50 +0200

    SyncLogger changed (should have been commited earlier)

--- data_synchronization/SyncLogger.py
+++ data_synchronization/SyncLogger.py
@@ -1,6 +1,9 @@
 import os
 import pandas as pd
 
+class FileAlreadySynchronized(Exception):
+    pass
+
 class SyncLogger:
     def __init__(self, log_table_path):
         """
@@ -11,31 +14,16 @@ class SyncLogger:
         """
         self.log_table_path = log_table_path
         self.columns = [
-            # the following are for the extraction
-            'path_bag', # path to the bag file processed
-            'output_path', # path to the output folder
-            'sync_strategy', # acc, LED
-            'date', # date of the recording (from the bag filename)
-            'acc_extraction_failed', # 0/1
-            'acc_avg_quality', # average quality of the frames
-            'first_frame_face_detected',
-            'face_location',
-            'face_patient_distance',
-            'Exception5000',
-            'unknown_error_extraction',
-            'relevant_wisci_files',
             # and these are for the synchronization
+            'mp4_name',
+            'mp4_length',
             'path_wisci',
-            'normalized_corr',
+            'corr',
             'lag',
-            'fps_bag',
-            'unknown_error_synchronization',
+            'sync_error_msg',
             'peaks_per_million',
             'best_second_largest_corr_peak',
-            'second_best_wisci_corr',
-            'second_best_wisci_peaks_per_million',
-            'second_best_wisci_path',
-            'synchronization_failed',
+            'sync_failed',
         ]
 
         if os.path.exists(log_table_path):
@@ -44,26 +32,26 @@ class SyncLogger:
             self.log_df = pd.DataFrame(columns=self.columns)
 
 
-    def process_new_file(self, path_bag):
+    def process_new_file(self, mp4_name):
         """
         Start logging a new file by appending a row with the filename.
         
         Parameters:
         - filename: The name of the file being processed.
         """
-        if not self.log_df['path_bag'].str.contains(path_bag).any():
+        if not self.log_df['mp4_name'].str.contains(mp4_name).any():
 
             filled_columns = {column: None for column in self.columns}
-            filled_columns['path_bag'] = path_bag
+            filled_columns['mp4_name'] = mp4_name
 
             new_row = pd.DataFrame([filled_columns])
             self.log_df = pd.concat([self.log_df, new_row], ignore_index=True)
         
         else:
-            raise ValueError(f"File {path_bag} already exists in the log.") # TODO: maybe specific exception
+            raise FileAlreadySynchronized(f"File {mp4_name} already exists in the log.")
 
     
-    def update_log(self, path_bag, column, value):
+    def update_log(self, mp4_name, column, value):
         """
         Update the log for a specific file and column.
         
@@ -73,10 +61,10 @@ class SyncLogger:
         - value: The new value for the column.
         """
         # Find the row index for the given filename. Assumes filename is unique for each row.
-        row_index = self.log_df[self.log_df['path_bag'] == path_bag].index[0]
+        row_index = self.log_df[self.log_df['mp4_name'] == mp4_name].index[0]
         self.log_df.at[row_index, column] = value
     
-    def get_value(self, path_bag, column):
+    def get_value(self, mp4_name, column):
         """
         Get the value of a specific column for a specific file.
         
@@ -88,7 +76,7 @@ class SyncLogger:
         - The value of the column for the given file.
         """
         # Find the row index for the given filename. Assumes filename is unique for each row.
-        row_index = self.log_df[self.log_df['path_bag'] == path_bag].index[0]
+        row_index = self.log_df[self.log_df['mp4_name'] == mp4_name].index[0]
         return self.log_df.at[row_index, column]
     
 
