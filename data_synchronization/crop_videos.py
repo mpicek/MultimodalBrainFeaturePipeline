9f2afb1ba37efffdee4b5264b0ad550e9b2cf9f1
Martin Picek <martinpicek4@gmail.com>
2024-05-13 10:29:48 +0200

    More logs in crop_video.py

--- data_synchronization/crop_videos.py
+++ data_synchronization/crop_videos.py
@@ -1,5 +1,5 @@
 ##################################################################################
-# Does not log the cropping process
+# Logs into cropping_log.csv if the bounding box data is corrupted
 # Crops only videos that have corresponding bounding box data
 # Skips already cropped videos
 ##################################################################################
@@ -8,6 +8,7 @@ import os
 import subprocess
 import numpy as np
 import argparse
+from tqdm import tqdm
 
 def crop_videos(mp4_folder, extraction_folder, output_folder):
     # Ensure output folder exists
@@ -15,18 +16,26 @@ def crop_videos(mp4_folder, extraction_folder, output_folder):
         os.makedirs(output_folder)
 
     # Iterate over videos in mp4_folder
-    for video_file in os.listdir(mp4_folder):
+    for video_file in tqdm(os.listdir(mp4_folder)):
         if video_file.endswith(".mp4"):
             mp4_filename = video_file[:-4]
             npy_file = os.path.join(extraction_folder, f"{mp4_filename}_bounding_box.npy")
 
             # Check if corresponding numpy file exists
             if os.path.exists(npy_file):
+                tqdm.write(f"Processing video {video_file}")
                 # Read bounding box coordinates from numpy file
-                bounding_box = np.load(npy_file)
+                bounding_box = np.load(npy_file, allow_pickle=True)
 
-                width = bounding_box[2] - bounding_box[0]
-                height = bounding_box[3] - bounding_box[1]
+                try:
+                    width = bounding_box[2] - bounding_box[0]
+                    height = bounding_box[3] - bounding_box[1]
+                except IndexError:
+                    tqdm.write(f"\tBounding box data is corrupted (patient was not probably detected). Logged into cropping_log.csv. Skipping the file.")
+                    # write it to a log file
+                    with open(os.path.join(output_folder, "cropping_log.csv"), "a") as f:
+                        f.write(f"{video_file}\n")
+                    continue
 
                 # Crop video using ffmpeg
                 input_path = os.path.join(mp4_folder, video_file)
@@ -34,15 +43,15 @@ def crop_videos(mp4_folder, extraction_folder, output_folder):
                 
                 # proceed if the file does not exist
                 if os.path.exists(output_path):
-                    print(f"Video {video_file} already cropped. Skipping.")
+                    tqdm.write(f"\tVideo already cropped. Skipping.")
                     continue
                 command = (f"ffmpeg -i {input_path} -loglevel warning -vf "
                            f"crop={width}:{height}:{bounding_box[0]}:{bounding_box[1]} "
                            f"{output_path}")
                 subprocess.run(command, shell=True)
-                print(f"Video {video_file} cropped and saved as {output_path}")
+                tqdm.write(f"\tVideo cropped and saved as {output_path}")
             else:
-                print(f"No bounding box data found for {video_file}")
+                tqdm.write(f"\tNo bounding box data found, skipping.")
 
 # Example usage
 mp4_folder = "/home/vita-w11/mpicek/processed/data/mp4_0"
